232ac727d0c1cded288e7c2aed81c0826032d703
fanxu wang <fanxu.wang@qashier.com>
2023-06-06 09:13:49 +0800

    wrap up the first version

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import PrmieVue from "primevue/config";
 import DataTable from "primevue/datatable";
-import Column from 'primevue/column';
+import Column from "primevue/column";
+import Dialog from "primevue/dialog";
+
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
 
 //global styles
 import "./assets/main.css";
@@ -12,3 +15,4 @@ application.use(router).mount("#app");
 application.use(PrmieVue);
 application.component("DataTable", DataTable);
 application.component("Column", Column);
+application.component("Dialog", Dialog);
--- src/views/newFriends/manageNewFriend.vue
+++ src/views/newFriends/manageNewFriend.vue
@@ -1,35 +1,79 @@
 <template>
   <div class="card">
-    <DataTable :value="products" tableStyle="min-width: 50rem">
-      <Column
-        v-for="col of columns"
-        :key="col.field"
-        :field="col.field"
-        :header="col.header"
-      ></Column>
+    <DataTable
+      paginator
+      :rows="10"
+      :value="documents"
+      tableStyle="min-width: 50rem"
+    >
+      <Column field="id" sortable hidden></Column>
+      <Column field="name" header="姓名" sortable></Column>
+      <Column field="contact" header="性别" sortable></Column>
+      <Column field="contact" header="联系方式" sortable></Column>
+      <Column field="userName" header="招待员" sortable></Column>
+      <Column field="recommender" header="介绍人" sortable></Column>
+
+      <Column field="DecisivePrayer" header="是否决志" sortable></Column>
+      <Column>
+        <template #body="slotProps">
+          <button @click="handleEdit(slotProps)">修改</button>
+        </template>
+      </Column>
     </DataTable>
   </div>
+  <Dialog
+    v-model:visible="displaySetting"
+    modal
+    header="修改"
+    :style="{ width: '50vw' }"
+  >
+    <label>姓名</label>
+    <input type="text" v-model="selectedRow.name" />
+
+    <label>性别</label>
+    <input type="text" v-model="selectedRow.gender" />
+
+    <label>联系方式</label>
+    <input type="text" v-model="selectedRow.contact" />
+
+    <label>招待员</label>
+    <input type="text" v-model="selectedRow.userName" />
+
+    <label>是否决志</label>
+    <input type="text" v-model="selectedRow.DecisivePrayer" />
+    <Button @click="handleUpdate(selectedRow)">提交</Button>
+  </Dialog>
 </template>
 
 <script setup>
+import getCollection from "@/composables/getCollection";
+import useDocument from "@/composables/useDoc";
 import { ref } from "vue";
+const { documents } = getCollection("newfriends");
+const displaySetting = ref(false);
+const selectedRow = ref(null);
 
-const products = ref({
-  id: "1000",
-  code: "f230fh0g3",
-  name: "Bamboo Watch",
-  description: "Product Description",
-  image: "bamboo-watch.jpg",
-  price: 65,
-  category: "Accessories",
-  quantity: 24,
-  inventoryStatus: "INSTOCK",
-  rating: 5,
-});
-const columns = [
-  { field: "code", header: "Code" },
-  { field: "name", header: "Name" },
-  { field: "category", header: "Category" },
-  { field: "quantity", header: "Quantity" },
-];
+const handleEdit = ({ data }) => {
+  selectedRow.value = data;
+  displaySetting.value = true;
+};
+
+const handleUpdate = async (selectedRow) => {
+  console.log("running");
+  const { updateDoc } = useDocument(`/newfriends/${selectedRow.id}`);
+  try {
+    await updateDoc({ ...selectedRow });
+  } catch (e) {
+    console.log("error", e);
+  }
+};
 </script>
+
+<style>
+.card {
+  background: var(--surface-card);
+  padding: 2rem;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+}
+</style>
